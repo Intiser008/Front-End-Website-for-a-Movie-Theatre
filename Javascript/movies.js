805b669f810e40aaeb3e09bd6098cfba9e8cbebb
Intiser008 <92087745+Intiser008@users.noreply.github.com>
2023-05-18 06:10:21 +0800

    Add files via upload

---
+++ Javascript/movies.js
@@ -0,0 +1,64 @@
+function getMovies() {
+    return [
+        {
+            id: 1,
+            type: "now",
+            thumbnail: "images/casablanca.png",
+            src: "https://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
+            name: "Casablanca",
+            cast: "Humphrey Bogart, Ingrid Bergmen",
+            director: "Michael Curtiz",
+            duration: 130
+        },
+        {
+            id: 2,
+            type: "now",
+            thumbnail: "images/bigbuck.jpg",
+            src: "https://courses.cs.cityu.edu.hk/cs2204/video/bigbuck-s.mp4",
+            name: "Big Buck Bunny",
+            cast: "Andreas Goralczyk",
+            director: "Sacha Goedegebure",
+            duration: 10
+        },
+        {
+            id: 3,
+            type: "now",
+            thumbnail: "images/barbecue.jpg",
+            src: "https://courses.cs.cityu.edu.hk/cs2204/video/barbecue.mp4",
+            name: "Barbeque Majik",
+            cast: "Idiot Sandwich",
+            director: "Gordon Ramsay",
+            duration: 39
+        },
+        {
+            id: 4,
+            type: "now",
+            thumbnail: "images/chocolate.jpg",
+            src: "https://courses.cs.cityu.edu.hk/cs2204/video/chocolate.mp4",
+            name: "Chocolates",
+            cast: "Charlie Bucket",
+            director: "Willy Wonka",
+            duration: 142
+        },
+        {
+            id: 5,
+            type: "upcoming",
+            thumbnail: "images/nature.jpg",
+            src: "https://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
+            name: "Nature",
+            cast: "Tarzan Smith",
+            director: "Ric Flair",
+            duration: 23
+        },
+        {
+            id: 6,
+            type: "upcoming",
+            thumbnail: "images/wonders.jpg",
+            src: "https://courses.cs.cityu.edu.hk/cs2204/video/wonders.mp4",
+            name: "Wonders",
+            cast: "Lionel Messi",
+            director: "Pep Guardiola",
+            duration: 90
+        }
+    ]
+}
